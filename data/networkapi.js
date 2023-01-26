0b2c7033cb2442cf560afdf0acde5b448356409a
doma2k <79820904+doma2k@users.noreply.github.com>
2023-01-26 11:47:53 +0000

    updates

--- data/networkapi.js
+++ data/networkapi.js
@@ -1,10 +1,12 @@
 // Network cosmos network api's.
 const juno = 'https://juno-api.polkachu.com/staking/validators';
 const persistence = 'https://persistence-api.polkachu.com/staking/validators';
-const api = [juno, persistence];
+const assetMantle = 'https://assetmantle-api.polkachu.com/staking/validators';
+const bitcanna = 'https://bitcanna-api.polkachu.com/staking/validators';
+const api = [juno, persistence, assetMantle, bitcanna];
 
 // DVS monikers.
-const monikers = ["POSTHUMAN∞DVS", "web34ever", "cyberG", "Imperator.co", "vbloher", "stakr-space", "Doma🏠Nodes", "👋 79anvi 🍀", "Colinka", "Paranormal Brothers", "ECO Stake 🌱"];
+const monikers = ["POSTHUMAN∞DVS", "web34ever", "cyberG", "Imperator.co", "vbloher", "stakr-space", "Doma🏠Nodes", "👋 79anvi 🍀", "Colinka", "Paranormal Brothers", "ECO Stake 🌱", "POSTHUMAN ꝏ DVS"];
 
 // Module export
 module.exports = { api, monikers };
--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const apiArray = require('./data/networkapi.js').api;
 getCurrentPrices(urls);
 getValidatorsList(apiArray);
 
+let tokenPrice = [];
+
 // Get validators data.
 function getValidatorsList(apiArray) {
     for (let i = 0; i < apiArray.length; i++) {
@@ -18,21 +20,26 @@ function getValidatorsList(apiArray) {
             });
             res.on('end', () => {
                 const validators = JSON.parse(body).result;
+                console.log('\n');
                 // Comparing JSON data to list of monikers.
+                console.log(names[i]);
                 for (let i = 0; i < dvs.length; i++) {
                     const filteredValidators = validators.filter(validator => validator.description.moniker === dvs[i]);
                     filteredValidators.forEach(validator => {
-                        console.log(validator.description.moniker + " : $" + ((validator.tokens / 1000000).toFixed(0) * junoPrice).toFixed(0));
+                        let x = (validator.tokens / 1000000).toFixed();
+                        // console.log(validator.description.moniker + " : $" + ((validator.tokens / 1000000).toFixed(0) * tokenPrice).toFixed(0));
+                        console.log(validator.description.moniker + " : " + x, "tokens");
                     });
                 }
-            });
-        });
+            }); 
+        }); 
     }
 }
 
+
 // Getting prices from gitcoin.
-let junoPrice;
-function getCurrentPrices(urls) {
+async function getCurrentPrices(urls)
+    {
     const coins = Object.keys(urls);
     for (let i = 0; i < urls.length; i++) {
         https.get(urls[i], (res) => {
@@ -46,14 +53,14 @@ function getCurrentPrices(urls) {
                 let tokenName = names[i];
                 //get the last element of the prices array.
                 const currentPrice = response.prices[response.prices.length - 1][1];
-                if (tokenName === 'JUNO') {
-                    junoPrice = currentPrice;
-                }
+                tokenPrice = currentPrice;
                 console.log(`${tokenName}: $${currentPrice.toFixed(2)}`);
             });
         });
     }
 }
+
+
 
 
 
