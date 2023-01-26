9fb9528545011f664e71b04c78ede3e35858fb8d
doma2k <79820904+doma2k@users.noreply.github.com>
2023-01-26 01:53:26 +0000

    moniker updates

--- data/networkapi.js
+++ data/networkapi.js
@@ -4,7 +4,7 @@ const persistence = 'https://persistence-api.polkachu.com/staking/validators';
 const api = [juno, persistence];
 
 // DVS monikers.
-const monikers = ["POSTHUMAN∞DVS", "web34ever", "cyberG", "Imperator.co", "vbloher", "stakr-space", "Doma🏠Nodes", "👋 79anvi 🍀", "Colinka", "Paranormal Brothers"];
+const monikers = ["POSTHUMAN∞DVS", "web34ever", "cyberG", "Imperator.co", "vbloher", "stakr-space", "Doma🏠Nodes", "👋 79anvi 🍀", "Colinka", "Paranormal Brothers", "ECO Stake 🌱"];
 
 // Module export
 module.exports = { api, monikers };
